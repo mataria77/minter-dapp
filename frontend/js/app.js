b327fff8d514ba95632755cde8d1112a91b113a8
Mataria <mihu_77@yahoo.com>
2022-08-17 07:49:50 +0300

    debug

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -156,7 +156,8 @@ async function checkChain() {
 
 async function loadInfo() {
   console.log(window.contract.methods);
-  window.info = await window.contract.methods.getInfo().call();
+  //window.info = await window.contract.methods.getInfo().call();
+  window.info = await window.contract.getInfo().call();
   const publicMintActive = await contract.methods.mintingActive().call();
   const presaleMintActive = await contract.methods.presaleActive().call();
   const mainHeading = document.getElementById("mainHeading");
