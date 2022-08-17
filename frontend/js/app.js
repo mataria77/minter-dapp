0165acc1240ef15ca158de3e64dda98dc723f887
Mataria <mihu_77@yahoo.com>
2022-08-17 12:35:12 +0300

    upd

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -229,7 +229,7 @@ async function loadInfo() {
     priceType = 'MATIC';
   }
   //const price = web3.utils.fromWei(info.deploymentConfig.mintPrice, 'ether');
-  const price = web3.utils.fromWei("0.001", 'ether');
+  const price = web3.utils.fromWei("0", 'ether');
   const pricePerMint = document.getElementById("pricePerMint");
   const maxPerMint = document.getElementById("maxPerMint");
   const totalSupply = document.getElementById("totalSupply");
